bd0d358262e48e401c600f1e312bf2c90daabc09
isk03276 <isk03276@naver.com>
2021-04-27 18:46:01 +0900

    logger 수정
    env 이름도 보이도록

--- common/logger.py
+++ common/logger.py
@@ -2,9 +2,10 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class TensorboardLogger:
-    def __init__(self, file_name, file_directory="log/"):
-        self.file_path = file_directory + file_name
+    def __init__(self, algo_name, env_name, file_directory="log/"):
+        self.file_path = file_directory + algo_name + env_name
         self.writer = SummaryWriter(self.file_path)
+        print(self.file_path)
 
     def log(self, name, data, step):
         self.writer.add_scalar(name, data, step)

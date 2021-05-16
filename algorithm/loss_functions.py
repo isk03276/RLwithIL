1029f60312e61aa57ed87e1dcf2af63d7632a73e
isk03276 <isk03276@naver.com>
2021-05-16 15:49:36 +0900

    Add loss functions class

---
+++ algorithm/loss_functions.py
@@ -0,0 +1,13 @@
+import torch
+from torch.nn import functional as F
+
+
+class RLLossFunctions:
+    @classmethod
+    def estimate_pg_loss(cls, ac_logprobs, values):
+        loss = -torch.stack(ac_logprobs) * values
+        return loss.mean()
+
+    @classmethod
+    def estimate_td_loss(values, returns):
+        loss = F.smooth_l1_loss(values, returns)
--- algorithm/reinforcement_learning/reinforce_algorithm.py
+++ algorithm/reinforcement_learning/reinforce_algorithm.py
@@ -2,6 +2,7 @@ import torch.optim as optim
 import torch
 
 from algorithm.reinforcement_learning.base_rl_algorithm import BaseRLAlgorithm
+from algorithm.loss_functions import RLLossFunctions
 from worker.single_worker import SingleWorker
 from common.logger import TensorboardLogger
 from common.rl_utils import RLUtils
@@ -27,17 +28,17 @@ class REINFORCEAlgorithm(BaseRLAlgorithm):
             trajectory = self.worker.sample_trajectory(-1, False)
             obs, acs, ac_logprobs, rews, nobs, dones, values = trajectory
 
-            loss = self.estimate_policy_loss(obs, acs, ac_logprobs, rews, nobs)
+            loss = self.estimate_policy_loss(ac_logprobs, rews)
             TorchUtils.update_network(self.policy_network_optimizer, loss)
 
             print(sum(rews), loss)
             self.logger.log("episodic reward", sum(rews), training_step)
             self.logger.log("policy loss", loss, training_step)
 
-    def estimate_policy_loss(self, obs, acs, ac_logprobs, rews, nobs):
+
+    def estimate_policy_loss(self, ac_logprobs, rews):
         returns = RLUtils.get_return(rews, self.gamma)
-        loss = -torch.stack(ac_logprobs) * returns
-        return loss.mean()
+        return RLLossFunctions.estimate_pg_loss(ac_logprobs, returns)
 
     def __str__(self):
         return "REINFORCE"
